19101c85ee9d217cbf8747b022bf85ff8d31f80f
evgeniy <pyankov.ev@gmail.com>
2021-04-20 00:05:20 +0300

    Use render.Bind to map body

--- httpd/handler/newsfeed_post.go
+++ httpd/handler/newsfeed_post.go
@@ -1,15 +1,12 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/render"
 	"github.com/pyankovzhe/chi-router/platform/newsfeed"
 )
-
-type itemRequest struct {
-	*newsfeed.Item
-}
 
 func NewsfeedPost(feed newsfeed.Adder) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -18,11 +15,25 @@ func NewsfeedPost(feed newsfeed.Adder) http.HandlerFunc {
 		data := &itemRequest{}
 
 		if err := render.Bind(r, data); err != nil {
-			http.Error(w, http.StatusText("Invalid request."), 400)
+			http.Error(w, http.StatusText(400), 400)
 			return
 		}
 
 		feed.Add(data.Item)
 		w.Write([]byte("OK"))
 	}
+}
+
+type itemRequest struct {
+	*newsfeed.Item
+}
+
+func (i *itemRequest) Bind(r *http.Request) error {
+	// a.Article is nil if no Article fields are sent in the request. Return an
+	// error to avoid a nil pointer dereference.
+	if i.Item == nil {
+		return errors.New("missing required Item fields.")
+	}
+
+	return nil
 }
